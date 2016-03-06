5ec67d6bd82c612e5756b3618e1bc5f9e885c4f4
xiaoxifei1223 <haochen9212@outlook.com>
2016-03-06 15:16:03 +0800

    Print

---
+++ src/org/lines/Datastructure/Datastructure.java
@@ -0,0 +1,11 @@
+package org.lines.Datastructure;
+
+public class Datastructure {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+
+	}
+
+}
